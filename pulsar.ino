be1854894a1a51f3ad781ae4f0ccf55fcc77ff79
Aindrea Bell <aindrea@openpots.org>
2023-07-10 13:58:48 +0100

    Add cadence handling

--- pulsar.ino
+++ pulsar.ino
@@ -27,82 +27,70 @@
 #define RM 0 // Ring mode on HIGH
 #define FR 1 // Reverse A and B (Tip and Ring pins on Ag1171) output polarities on LOW
 #define SHK 2 // Indicates off-hook condition when HIGH
-#define DEBUG 0 // Debug pin
+
+/* --- TEST CODE --- */
+#define RING_MODE_SWITCH 3 // Set US or UK ring based on pin
+#define UK_RING LOW
+#define US_RING HIGH
+#define DEBUG 4 // Debug pin
+/* --- TEST CODE --- */
 
 typedef struct Cadence {
   byte freq;
   byte numRings;
-  byte onTime_periods;
-  byte offTime_periods;
-  byte pauseTime_periods;
+  int onTime;
+  int offTime;
+  int pauseTime;
 } Cadence;
 
-struct CadenceState {
-  byte ticksRemaining;
-  byte step;
-  bool on;
-} cadenceState = {0, 0 , false};
+Cadence ukRing = {25, 2, 400, 200, 2000};
+Cadence usRing = {20, 1, 2000, 0, 4000};
 
-byte postscalerCounter = 0;
-byte postscalerTicks = 0;
-byte prescalerTicks = 0;
-bool generateRingSignal = false;
-bool ringSignalRunning = false;
+struct RingSignalState {
+  byte postscalerCounter;
+  byte postscalerTicks;
+  byte prescalerTicks;
+  bool generateRingSignal;
+  bool ringSignalRunning;
+  bool updateNeeded;
+} ringSignalState = {0, 0, 0, false, false, false};
+
+struct CadenceState {
+  byte numRings;
+  byte onEdges;
+  byte offEdges;
+  byte pauseEdges;
+  byte edgesRemaining;
+  int totalSteps;
+  int currentStep;
+} cadenceState = {0, 0, 0, 0, 255, 0, 0};
+
+struct LineState {
+  bool offHook;
+  bool ringing;
+} lineState = {false, false};
 
 ISR(TIMER0_COMPB_vect) {
-  if(postscalerCounter == postscalerTicks - 1) {
-    // Last full postscaler tick has occurred, next interval should be
-    // truncated to the remaining number of prescaler ticks
-    OCR0A = prescalerTicks - 1;
-    OCR0B = OCR0A;
-    postscalerCounter++;
-  }
-  else if(postscalerCounter == postscalerTicks || postscalerTicks == 0) {
-    // All ticks have occurred, either generate an edge or stop generating
-    // ringing signal
-    if(generateRingSignal == true) {
-      // Generate an edge
-      digitalWrite(FR, digitalRead(FR) ^ 1);
-    }
-    else {
-      // Don't generate an edge, make sure FR is set HIGH
-      digitalWrite(FR, HIGH);
-      // Tell main program we've now stopped generating a ring signal
-      ringSignalRunning = false;
-    }
-
-    if(postscalerTicks > 0) {
-      // Reset postscaler if we're using it
-      OCR0A = T0_POSTSCALER - 1;
-      OCR0B = OCR0A;
-      postscalerCounter = 0;
-    }
-
-    // Update cadence timer
-    /*cadenceState.ticksRemaining = cadenceState.ticksRemaining == 0
-                                    ? 0 : cadenceState.ticksRemaining - 1;*/
-  }
-  else {
-    // Not at an interesting tick yet
-    postscalerCounter++;
-  }
+  ringSignalState.updateNeeded = true;
 }
 
+/* --- TEST CODE --- */
 void debugFlash(int val) {
   bool oldVal = digitalRead(DEBUG);
   digitalWrite(DEBUG, LOW);
-  delay(1000);
+  delay(5);
   for(int i = 0; i < val; i++) {
     digitalWrite(DEBUG, HIGH);
-    delay(10);
+    delay(5);
     digitalWrite(DEBUG, LOW);
-    delay(10);
+    delay(5);
   }
-  delay(1000);
   digitalWrite(DEBUG, oldVal);
 }
+/* --- TEST CODE --- */
 
 void startTimer0() {
+  // Idempotent if timer already running
   GTCCR &= ~T0_STOP;
 }
 
@@ -133,7 +121,7 @@ void initialiseTimer0() {
   TCCR0B |= T0_PRESCALER_1024;
 
   // Reset postscaler
-  postscalerCounter = 0;
+  ringSignalState.postscalerCounter = 0;
 
   // Enable interrupt on Compare Match B
   TIMSK |= (1 << OCIE0B);
@@ -148,6 +136,13 @@ void setup() {
   // Initialise Ag1171
   digitalWrite(RM, LOW);
   digitalWrite(FR, HIGH);
+
+  initialiseTimer0();
+
+  /* --- TEST CODE --- */
+  pinMode(RING_MODE_SWITCH, INPUT);
+  pinMode(DEBUG, OUTPUT);
+  /* --- TEST CODE --- */
 }
 
 void setupRingSignal(byte freq) {
@@ -156,39 +151,35 @@ void setupRingSignal(byte freq) {
   float edgeFreq = freq * 2;
   float targetTicks = T0_POSTSCALED_F / edgeFreq;
   // Truncate target ticks to get quotient
-  postscalerTicks = byte(targetTicks);
+  ringSignalState.postscalerTicks = byte(targetTicks);
   // Use remainder to get number of prescaler ticks
-  prescalerTicks = byte((targetTicks - postscalerTicks) * T0_POSTSCALER);
+  ringSignalState.prescalerTicks = byte((targetTicks - ringSignalState.postscalerTicks) * T0_POSTSCALER);
 
   // Special case for 0 postscaler ticks, as we start off on prescaler ticks
-  if(postscalerTicks == 0) {
-    OCR0A = prescalerTicks - 1;
+  if(ringSignalState.postscalerTicks == 0) {
+    OCR0A = ringSignalState.prescalerTicks - 1;
     OCR0B = OCR0A;
   }
 }
 
 void startRingSignal() {
   // Tell Timer0 ISR it's okay to generate a signal
-  generateRingSignal = true;
-  ringSignalRunning = true;
+  ringSignalState.generateRingSignal = true;
+  ringSignalState.ringSignalRunning = true;
   // Put Ag1171 into ringing mode
   digitalWrite(RM, HIGH);
   // Start ringing signal on Timer0
   startTimer0();
 }
 
+// Stop generating a ringing signal but keep the timer going
 void stopRingSignal() {
-  bool noop = 0;
-
-  // Tell Timer0 ISR to stop ringing signal at the end of current cycle
-  generateRingSignal = false;
+  // Tell update function not to generate a signal anymore at the end of current cycle
+  ringSignalState.generateRingSignal = false;
 
   // Wait for Timer0 to switch FR permanently HIGH then turn off ringing mode
-  while(true) {
-    // workaround for compiler incorrectly removing no-op loop
-    if(ringSignalRunning == true){
-      break;
-    }
+  while(ringSignalState.ringSignalRunning == true) {
+    updateRingSignal();
   }
 
   // Ag1171 needs a delay of > 10 ms between FR permanently HIGH and RM LOW
@@ -196,35 +187,123 @@ void stopRingSignal() {
   digitalWrite(RM, LOW);
 }
 
-void startRinging(int freq) {
-  setupRingSignal(freq);
+void updateRingSignal() {
+  // Do nothing until timer has fired
+  if(ringSignalState.updateNeeded == true) {
+    // Last full postscaler tick
+    if(ringSignalState.postscalerCounter == ringSignalState.postscalerTicks - 1) {
+      // Next interval should be truncated to the remaining number of prescaler
+      // ticks
+      OCR0A = ringSignalState.prescalerTicks - 1;
+      OCR0B = OCR0A;
+      ringSignalState.postscalerCounter++;
+    }
+    // Final postscaler tick (truncated) or not using postscaler
+    else if(ringSignalState.postscalerCounter == ringSignalState.postscalerTicks || ringSignalState.postscalerTicks == 0) {
+      // All ticks have occurred, either generate an edge or stop generating
+      // ringing signal
+      if(ringSignalState.generateRingSignal == true) {
+        // Generate an edge
+        digitalWrite(FR, digitalRead(FR) ^ 1);
+      }
+      else {
+        // Don't generate an edge, make sure FR is set HIGH
+        digitalWrite(FR, HIGH);
+        // Tell main program we've now stopped generating a ring signal
+        ringSignalState.ringSignalRunning = false;
+      }
+
+      if(ringSignalState.postscalerTicks > 0) {
+        // Reset postscaler if we're using it
+        OCR0A = T0_POSTSCALER - 1;
+        OCR0B = OCR0A;
+        ringSignalState.postscalerCounter = 0;
+      }
+
+      // Update cadence timer
+      cadenceState.edgesRemaining = cadenceState.edgesRemaining == 0
+                                      ? 0
+                                      : cadenceState.edgesRemaining - 1;
+    }
+    // Not at an interesting tick yet
+    else {
+      ringSignalState.postscalerCounter++;
+    }
+
+    ringSignalState.updateNeeded = false;
+  }
+}
+
+void setupCadence(Cadence *cadence) {
+  cadenceState.numRings = cadence->numRings;
+  int edgeTime = 1000 / (cadence->freq * 2);
+  cadenceState.onEdges = cadence->onTime / edgeTime;
+  cadenceState.offEdges = cadence->offTime / edgeTime;
+  cadenceState.pauseEdges = cadence->pauseTime / edgeTime;
+  cadenceState.edgesRemaining = cadenceState.onEdges;
+  cadenceState.totalSteps = 2 * cadence->numRings;
+  cadenceState.currentStep = 0;
+}
+
+void updateCadence() {
+  // Do nothing until end of current cadence step
+  if(cadenceState.edgesRemaining == 0) {
+    // Next step is last step ("Pause")
+    if(cadenceState.currentStep == cadenceState.totalSteps - 2) {
+      stopRingSignal();
+      cadenceState.edgesRemaining = cadenceState.pauseEdges;
+    }
+    // Next step is "Off" step
+    else if(cadenceState.currentStep % 2 == 0) {
+      stopRingSignal();
+      cadenceState.edgesRemaining = cadenceState.offEdges;
+    }
+    // Next step is "On" step
+    else if(cadenceState.currentStep % 2 == 1) {
+      startRingSignal();
+      cadenceState.edgesRemaining = cadenceState.onEdges;
+    }
+
+    cadenceState.currentStep = (cadenceState.currentStep + 1) % cadenceState.totalSteps;
+  }
+}
+
+void startRinging(Cadence *cadence) {
+  setupRingSignal(cadence->freq);
+  setupCadence(cadence);
+  lineState.ringing = true;
   startRingSignal();
 }
 
 void stopRinging() {
   stopRingSignal();
   stopTimer0();
+  lineState.ringing = false;
 }
 
-void advanceCadence() {
+void updateLine() {
+  lineState.offHook = digitalRead(SHK);
 
+  if(lineState.ringing == true && lineState.offHook == true) {
+    stopRinging();
+  }
 }
 
 void loop() {
-  startRinging(25);
-  for(int i = 0; i < 4; i++) {
-    delay(500);
-    stopRingSignal();
-    delay(500);
-    startRingSignal();
+  updateLine();
+  updateRingSignal();
+  updateCadence();
+
+  /* --- TEST CODE --- */
+  if(lineState.ringing == false && lineState.offHook == false) {
+    delay(1000);
+    bool ringMode = digitalRead(RING_MODE_SWITCH);
+    if(ringMode == UK_RING) {
+      startRinging(&ukRing);
+    }
+    else {
+      startRinging(&usRing);
+    }
   }
-  stopRinging();
-  delay(500);
-  startRinging(5);
-  for(int i = 0; i < 4; i++) {
-    delay(500);
-    stopRingSignal();
-    delay(500);
-    startRingSignal();
-  }
+  /* --- TEST CODE --- */
 }
