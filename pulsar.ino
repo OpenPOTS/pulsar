21e28eb149b028c7ddde67b7e3b643824626a0ad
Aindrea Bell <aindrea@openpots.org>
2023-07-10 13:58:47 +0100

    Add license text to source

--- pulsar.ino
+++ pulsar.ino
@@ -1,3 +1,20 @@
+/*  Pulsar - Control a landline phone via USB using an Ag1171 SLIC
+    Copyright (C) 2023  Aindrea Bell
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #define F_CPU 16500000UL // CPU frequency
 #define T0_CTC_MODE (1 << WGM01)
 #define T0_PRESCALER  1024
